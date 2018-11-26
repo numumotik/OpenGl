1858d0e78c4ccb949b449b5cb688a77fa143213b
Dmitry Volodin <mr.molkree@gmail.com>
2018-11-27 02:11:24 +0300

    refactoring

--- Graphics/lab11/main.cpp
+++ Graphics/lab11/main.cpp
@@ -1,18 +1,26 @@
-#include<Windows.h>    
+#include<Windows.h>
 // first include Windows.h header file which is required    
-#include<stdio.h>    
+#include<cstdio>
 #include<gl/GL.h>   // GL.h header file    
 #include<gl/GLU.h> // GLU.h header file    
 #include<gl/glut.h>  // glut.h header file from freeglut\include\GL folder    
-#include<conio.h>    
-#include<stdio.h>    
-#include<math.h>    
-#include<string.h>    
+#include<conio.h>
+#include<cmath>
+#include<cstring>
 #include<SOIL.h>
 
 unsigned char* image;
 GLuint texture;
 GLuint texture2;
+
+const double pi = 3.14159265358979323846;
+float camera_angle = 50;
+float camera_pos = 5;
+float camera_rad = 10;
+float car_angle = 0;
+int is_ahead = 0;
+int is_back = 0;
+const GLfloat no_mat[] = { 0.0, 0.0, 0.0, 1.0 };
 
 void makeTextureImage()
 {
@@ -36,13 +44,6 @@ void makeTextureImage()
 	);
 }
 
-float camera_angle = 50;
-float camera_pos = 5;
-float camera_rad = 10;
-float car_angle = 0;
-int is_ahead = 0;
-int is_back = 0;
-
 // Инициализация
 void init(void)
 {
@@ -50,33 +51,34 @@ void init(void)
 	glEnable(GL_NORMALIZE);
 	glEnable(GL_COLOR_MATERIAL);
 	
+	// Street lights
 	const GLfloat light_ambient[] = { 0.0, 0.0, 0.0, 1 };
 	const GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
 	const GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
 	glLightfv(GL_LIGHT1, GL_AMBIENT, light_ambient);
 	glLightfv(GL_LIGHT1, GL_DIFFUSE, light_diffuse);
 	glLightfv(GL_LIGHT1, GL_SPECULAR, light_specular);
-	//glEnable(GL_LIGHT1);
+	
 	glLightfv(GL_LIGHT2, GL_AMBIENT, light_ambient);
 	glLightfv(GL_LIGHT2, GL_DIFFUSE, light_diffuse);
 	glLightfv(GL_LIGHT2, GL_SPECULAR, light_specular);
-	//glEnable(GL_LIGHT2);
+	
 	glLightfv(GL_LIGHT3, GL_AMBIENT, light_ambient);
 	glLightfv(GL_LIGHT3, GL_DIFFUSE, light_diffuse);
 	glLightfv(GL_LIGHT3, GL_SPECULAR, light_specular);
-	//glEnable(GL_LIGHT3);
+	
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_LIGHTING);
 
 	makeTextureImage();	
-
-	//glEnable(GL_LIGHT0);
+	
+	// Camera light
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
 	glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, 30.0);
-	//glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 1);
 	glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.1);
 	glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, 0.01);
 	
+	// Car lights
 	const GLfloat car_diffuse[] = { 1.0, 1.0, 0, 0 };
 	const GLfloat car_ambient[] = { 0.0, 0.0, 0.0, 1 };
 	const GLfloat car_specular[] = { 1.0, 1.0, 0, 1.0 };
@@ -90,7 +92,7 @@ void init(void)
 	glLightfv(GL_LIGHT5, GL_AMBIENT, car_ambient);
 	glLightfv(GL_LIGHT5, GL_DIFFUSE, car_diffuse);
 	glLightf(GL_LIGHT5, GL_SPOT_CUTOFF, 45.0);
-	glLightf(GL_LIGHT5, GL_SPOT_EXPONENT,1.0);
+	glLightf(GL_LIGHT5, GL_SPOT_EXPONENT, 1.0);
 	glLightf(GL_LIGHT5, GL_CONSTANT_ATTENUATION, 0.5);
 	glLightf(GL_LIGHT5, GL_LINEAR_ATTENUATION, 0.3);
 	glLightf(GL_LIGHT5, GL_QUADRATIC_ATTENUATION, 0.1);
@@ -109,11 +111,11 @@ void reshape(int w, int h)
 
 void drawLights()
 {
-	GLfloat no_mat[] = { 0.0,0.0,0.0,1.0 };
-	GLfloat mat_emission[] = { 1,1,1,0.0 };
-	const GLfloat position[] = { 0,4,0,1 };
+	const GLfloat mat_emission[] = { 1, 1, 1, 0.0 };
+	const GLfloat position[] = { 0, 4, 0, 1 };
 	glColor3f(1.0, 1.0, 1.0);
 
+	// Street lights
 	glPushMatrix();
 	glTranslatef(-5, 0, 0);
 	glLightfv(GL_LIGHT1, GL_POSITION, position);
@@ -125,7 +127,7 @@ void drawLights()
 
 	glPushMatrix();
 	if (glIsEnabled(GL_LIGHT1))
-			glMaterialfv(GL_FRONT, GL_EMISSION, mat_emission);
+		glMaterialfv(GL_FRONT, GL_EMISSION, mat_emission);
 	else glMaterialfv(GL_FRONT, GL_EMISSION, no_mat);
 	glColor3f(1.0, 1.0, 0.0);
 	glTranslatef(-5, 4, 0);
@@ -170,12 +172,12 @@ void drawLamps()
 
 double gr_cos(float angle) noexcept
 {
-	return cos(angle / 180 * 3.14);
+	return cos(angle / 180 * pi);
 }
 
 double gr_sin(float angle) noexcept
 {
-	return sin(angle / 180 * 3.14);
+	return sin(angle / 180 * pi);
 }
 
 void setCamera()
@@ -183,12 +185,12 @@ void setCamera()
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	const double x = camera_rad * gr_cos(camera_angle);
-	const double y = camera_rad * gr_sin(camera_angle);
-	gluLookAt(x, camera_pos, y, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	const double z = camera_rad * gr_sin(camera_angle);
+	gluLookAt(x, camera_pos, z, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 	
-	GLfloat spot_position[] = { x, camera_pos, y , 1};
-	GLfloat spot_direction[] = { -x, -camera_pos, -y };
-	float length = sqrt(x*x + camera_pos*camera_pos + y*y);
+	const GLfloat spot_position[] = { x, camera_pos, z , 1};
+	GLfloat spot_direction[] = { -x, -camera_pos, -z };
+	const float length = sqrt(x * x + static_cast<double>(camera_pos) * static_cast<double>(camera_pos) + z * z);
 	if (length != 0)
 	{
 		spot_direction[0] /= length;
@@ -204,6 +206,7 @@ void tryingTexturedCube(GLfloat size)
 	glBindTexture(GL_TEXTURE_2D, texture2);
 	glEnable(GL_TEXTURE_2D);
 	glBegin(GL_QUADS);
+
 	// Front Face
 	glNormal3f(0, 0, 1); glTexCoord2f(0.0f, 0.0f); glVertex3f(-size, -size, size);  // Bottom Left Of The Texture and Quad
 	glNormal3f(0, 0, 1); glTexCoord2f(1.0f, 0.0f); glVertex3f(size, -size, size);  // Bottom Right Of The Texture and Quad
@@ -216,13 +219,13 @@ void tryingTexturedCube(GLfloat size)
 	glNormal3f(0, 0, -1); glTexCoord2f(0.0f, 1.0f); glVertex3f(size, size, -size);  // Top Left Of The Texture and Quad
 	glNormal3f(0, 0, -1); glTexCoord2f(0.0f, 0.0f); glVertex3f(size, -size, -size);  // Bottom Left Of The Texture and Quad
 	
-	 // Top Face
+	// Top Face
 	glNormal3f(0, 1, 0); glTexCoord2f(0.0f, 1.0f); glVertex3f(-size, size, -size);  // Top Left Of The Texture and Quad
 	glNormal3f(0, 1, 0); glTexCoord2f(0.0f, 0.0f); glVertex3f(-size, size, size);  // Bottom Left Of The Texture and Quad
 	glNormal3f(0, 1, 0); glTexCoord2f(1.0f, 0.0f); glVertex3f(size, size, size);  // Bottom Right Of The Texture and Quad
 	glNormal3f(0, 1, 0); glTexCoord2f(1.0f, 1.0f); glVertex3f(size, size, -size);  // Top Right Of The Texture and Quad
 	
-	 // Bottom Face
+	// Bottom Face
 	glNormal3f(0, -1, 0); glTexCoord2f(1.0f, 1.0f); glVertex3f(-size, -size, -size);  // Top Right Of The Texture and Quad
 	glNormal3f(0, -1, 0); glTexCoord2f(0.0f, 1.0f); glVertex3f(size, -size, -size);  // Top Left Of The Texture and Quad
 	glNormal3f(0, -1, 0); glTexCoord2f(0.0f, 0.0f); glVertex3f(size, -size, size);  // Bottom Left Of The Texture and Quad
@@ -243,7 +246,6 @@ void tryingTexturedCube(GLfloat size)
 	glDisable(GL_TEXTURE_2D);
 }
 
-
 float x = 0; float z = 0;
 float SPEED = 0.1;
 float wheel_angle = 0;
@@ -254,12 +256,12 @@ void drawWheel()
 	glutSolidCube(0.1);
 	glutSolidTorus(0.15, 0.25, 10, 10);
 }
+
 void drawCarLights()
 {
-	GLfloat no_mat[] = { 0.0,0.0,0.0,1.0 };
-	GLfloat mat_emission[] = { 1,1,0,0.0 };
-	GLfloat spot_position[] = { 0, 0, 0, 1 };
-	GLfloat spot_direction[] = { 0, 0, 1};
+	const GLfloat mat_emission[] = { 1, 1, 0, 0.0 };
+	const GLfloat spot_position[] = { 0, 0, 0, 1 };
+	const GLfloat spot_direction[] = { 0, 0, 1};
 
 	glTranslatef(0, -0.15, 1.45);
 	glColor3f(1, 1, 0);
@@ -297,25 +299,24 @@ void drawCar()
 	glTranslatef(x, 0.9, z);
 	glRotatef(car_angle, 0, 1, 0);
 	
-	//корпус
+	// корпус
 	glPushMatrix();
 	glColor3f(0.8, 0.8, 0.8);
-		glTranslatef(0, 0, 1.2);
-		glutSolidCube(0.5);
-		glTranslatef(0, 0, -1.2);
+	glTranslatef(0, 0, 1.2);
+	glutSolidCube(0.5);
+	glTranslatef(0, 0, -1.2);
 	glScalef(1, 1, 2);
 	tryingTexturedCube(0.5);
-	//glutSolidCube(1);
 	glPopMatrix();
 	
 	glPushMatrix();
 	drawCarLights();
 	glPopMatrix();
 
-
 	glTranslatef(0, -0.6, 0);
 	glColor3f(0.2, 0.2, 0.2);
-	//колеса
+
+	// колеса
 	glPushMatrix();
 	glTranslatef(0.4, 0, 0.5);
 	drawWheel();
@@ -403,6 +404,7 @@ void drawRandomObstacles()
 
 	glPopMatrix();
 }
+
 // Отображение
 void display(void)
 {
@@ -445,11 +447,15 @@ void specialKeys(int key, int x, int y)
 			break;
 		case GLUT_KEY_F4:
 			if (glIsEnabled(GL_LIGHT4))
-				glDisable(GL_LIGHT4);
-			else glEnable(GL_LIGHT4);
-			if (glIsEnabled(GL_LIGHT5))
-				glDisable(GL_LIGHT5);
-			else glEnable(GL_LIGHT5);
+            {
+                glDisable(GL_LIGHT4);
+                glDisable(GL_LIGHT5);
+            }
+			else
+            {
+                glEnable(GL_LIGHT4);
+                glEnable(GL_LIGHT5);
+            }
 			break;
 	}
 	glutPostRedisplay();
