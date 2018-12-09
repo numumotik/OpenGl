d8e3974dbdabd8c9a3b7db82b6f5306b339bcb5c
numumotik <numumotik@mail.ru>
2018-12-09 14:59:49 +0300

    все плохо

--- Graphics/lab12/main.cpp
+++ Graphics/lab12/main.cpp
@@ -7,11 +7,11 @@
 #include<conio.h>
 #include<math.h>
 #include<string.h>
-//#include<SOIL.h>
+#include<SOIL.h>
 #include <vector>
+
 unsigned char* image;
 GLuint texture;
-
 const double pi = 3.14159265358979323846;
 int light_num = 0;
 std::vector<float> light_angle { 0, 90, 0 };
@@ -23,27 +23,110 @@ int is_back = 0;
 int width = 0, height = 0;
 int model_num = 0;
 
-/*void makeTextureImage()
+struct vec3
+{
+	float x;
+	float y;
+	float z;
+};
+
+struct vec2
+{
+	float x;
+	float y;
+};
+
+bool loadOBJ(const char* path, std::vector<vec3>& out_vertices, std::vector<vec2>& out_uvs, std::vector<vec3>& out_normals)
+{
+	std::vector<unsigned int> vertexIndices, uvIndices, normalIndices;
+	std::vector<vec3> temp_vertices;
+	std::vector<vec2> temp_uvs;
+	std::vector<vec3> temp_normals;
+	FILE * file = fopen(path, "r");
+	if (file == NULL) {
+		printf("Impossible to open the file !\n");
+		return false;
+	}
+
+	while (1) {
+
+		char lineHeader[128];
+		// read the first word of the line
+		int res = fscanf(file, "%s", lineHeader);
+		if (res == EOF)
+			break; // EOF = End Of File. Quit the loop.
+		if (strcmp(lineHeader, "v") == 0) {
+			vec3 vertex;
+			fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
+			temp_vertices.push_back(vertex);
+		}
+		else if (strcmp(lineHeader, "vt") == 0) {
+			vec2 uv;
+			fscanf(file, "%f %f\n", &uv.x, &uv.y);
+			temp_uvs.push_back(uv);
+		}
+		else if (strcmp(lineHeader, "vn") == 0) {
+			vec3 normal;
+			fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
+			temp_normals.push_back(normal);
+		}
+		else if (strcmp(lineHeader, "f") == 0) {
+			std::string vertex1, vertex2, vertex3;
+			unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
+			int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
+			if (matches != 9) {
+				printf("File can't be read by our simple parser : ( Try exporting with other options\n");
+				return false;
+			}
+			vertexIndices.push_back(vertexIndex[0]);
+			vertexIndices.push_back(vertexIndex[1]);
+			vertexIndices.push_back(vertexIndex[2]);
+			uvIndices.push_back(uvIndex[0]);
+			uvIndices.push_back(uvIndex[1]);
+			uvIndices.push_back(uvIndex[2]);
+			normalIndices.push_back(normalIndex[0]);
+			normalIndices.push_back(normalIndex[1]);
+			normalIndices.push_back(normalIndex[2]);
+		}
+	}
+
+	// For each vertex of each triangle
+	for (unsigned int i = 0; i < vertexIndices.size(); i++) {
+		unsigned int vertexIndex = vertexIndices[i];
+		vec3 vertex = temp_vertices[vertexIndex - 1];
+		out_vertices.push_back(vertex);
+	}
+	
+	for (unsigned int i = 0; i < uvIndices.size(); i++) {
+		unsigned int uvIndex = uvIndices[i];
+		vec2 vertex = temp_uvs[uvIndex - 1];
+		out_uvs.push_back(vertex);
+	}
+
+	for (unsigned int i = 0; i < normalIndices.size(); i++) {
+		unsigned int normalIndex = normalIndices[i];
+		vec3 normal = temp_normals[normalIndex - 1];
+		out_normals.push_back(normal);
+	}
+	
+	return true;
+}
+
+void makeTextureImage()
 {
 	texture = SOIL_load_OGL_texture
 	(
-		"texture2.jpg",
+		"african_head_SSS.jpg",
 		SOIL_LOAD_AUTO,
 		SOIL_CREATE_NEW_ID,
 		SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT
 	);
+	glBindTexture(GL_TEXTURE_2D, texture);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	texture2 = SOIL_load_OGL_texture
-	(
-		"car_texture.jpg",
-		SOIL_LOAD_AUTO,
-		SOIL_CREATE_NEW_ID,
-		SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT
-	);
-}*/
+}
 
 // Инициализация
 void init(void)
@@ -145,7 +228,13 @@ float SPEED = 0.1;
 
 void draw_simple_model()
 {
-	glutSolidCube(5);
+	//glutSolidCube(5);
+	std::vector< vec3 > vertices;
+	std::vector< vec2 > uvs;
+	std::vector< vec3 > normals; // Won't be used at the moment.
+	bool res = loadOBJ("cube_obj.txt", vertices, uvs, normals);
+	
+	//glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
 }
 
 void draw_head()
